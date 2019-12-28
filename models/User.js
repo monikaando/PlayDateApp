fb77359ac6f046937e9ed5aca5cabbf7af309e5a
Monika <mswidzinska87@gmail.com>
2019-12-28 17:30:21 +0100

    added models User

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Sechema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    profile_pic: { type: String, default: '/images/default_user.png' },
+    guardian: { type: String, enum: ['mother', 'father', 'sister', 'brother', 'baby sitter', 'other'] },
+    phone: { type: String, required: false, validate: /^\d{10}$/ },
+    email: { type: String, required: true },
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
