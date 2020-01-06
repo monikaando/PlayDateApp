a6ec08412f6b9ae224ef0b1fd7aebc1a100511d0
Monika <mswidzinska87@gmail.com>
2020-01-06 23:19:30 +0100

    styling driends list

--- models/Caretaker.js
+++ models/Caretaker.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 const caretakerSchema = new Schema({
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
-    // profile_pic: { type: String, default: '/images/default_user.png' }, //Do we need that?
     availabledays: [{ type: String }],
     relation: { type: String },
     address: { type: String },
