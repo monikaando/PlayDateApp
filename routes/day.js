90dbcea8a9850a15d27a0ddd514cc406ea6b98b0
almightyblob <dominik.antunovic@gmail.com>
2020-01-07 13:36:44 +0100

    working on adding caretakers to day filter

--- routes/day.js
+++ routes/day.js
@@ -1,26 +1,19 @@
 const express = require("express");
 const app = express();
 const Child = require("../models/Child");
-const Caretaker = require("../models/Caretaker");
 const createError = require('http-errors');
 
 app.get("/:day", (req, res) => {
-    debugger;
     Child.find({availabledays: req.params.day})
-    .then(friendsResults => {
-        req.session.friends = friendsResults;
-        Caretaker.findOne({availabledays: req.params.day})
-    })
-    .then(caretaker => {
-        res.render("friends/friendslist", {friends: req.session.friends, caretaker});
+    .populate("caretaker")
+    .then(friends => {
+        friends.filter((friend)=> returnfriend.caretaker.availabledays.includes(req.params.day)
+        })
+        res.render("friends/friendsbyday", {friends});
     })
     .catch(error => {
         next(error);
     });
 });
-
-
-
-
 
 module.exports = app;
