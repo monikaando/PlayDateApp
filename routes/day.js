e0df837c301349bb88eb61c2a29a1d204d2fe05a
almightyblob <dominik.antunovic@gmail.com>
2020-01-07 14:36:06 +0100

    tiny fixed

--- routes/day.js
+++ routes/day.js
@@ -7,8 +7,6 @@ app.get("/:day", (req, res) => {
     Child.find({availabledays: req.params.day})
     .populate("caretaker")
     .then(friends => {
-        friends.filter((friend)=> returnfriend.caretaker.availabledays.includes(req.params.day)
-        })
         res.render("friends/friendsbyday", {friends});
     })
     .catch(error => {
@@ -17,3 +15,6 @@ app.get("/:day", (req, res) => {
 });
 
 module.exports = app;
+
+// friends.filter((friend)=> returnfriend.caretaker.availabledays.includes(req.params.day)
+// ^^  Jurgen's Code for looping through friends and find the caretakers
