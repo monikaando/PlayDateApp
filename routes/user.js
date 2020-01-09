5aee59be9ee631e062eb9dad005b75165c54bfe3
Monika <mswidzinska87@gmail.com>
2020-01-09 16:31:20 +0100

    profile without photo

--- routes/user.js
+++ routes/user.js
@@ -4,21 +4,21 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const User = require("../models/User");
 
-app.get("/upload-profile-pic", (req, res) => {
+app.get("/profile", (req, res) => {
     res.render("user/profile-picture");
 })
 
-app.post("/upload-profile-pic", upload.single('profile-picture'), (req, res) => {
-    User.findByIdAndUpdate(req.session.currentUser._id, {
-            profile_pic: req.file.filename
-        })
-        .then((user) => {
-            req.session.currentUser.profile_pic = req.file.filename;
-            res.redirect("/user/upload-profile-pic")
-        })
-        .catch((err) => {
-            res.send("err", err)
-        })
-})
+// app.post("/upload-profile-pic", upload.single('profile-picture'), (req, res) => {
+//     User.findByIdAndUpdate(req.session.currentUser._id, {
+//             profile_pic: req.file.filename
+//         })
+//         .then((user) => {
+//             req.session.currentUser.profile_pic = req.file.filename;
+//             res.redirect("/user/upload-profile-pic")
+//         })
+//         .catch((err) => {
+//             res.send("err", err)
+//         })
+// })
 
 module.exports = app;
