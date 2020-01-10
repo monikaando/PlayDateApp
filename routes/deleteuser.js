e117b6e5fac4c540a933a9740ee44b2c478c887a
Monika <mswidzinska87@gmail.com>
2020-01-10 15:21:39 +0100

    destroy session

--- routes/deleteuser.js
+++ routes/deleteuser.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const Child = require("../models/Child"); 
+const Child = require("../models/Child");
 const cloudinary = require('cloudinary');
 const Caretaker = require("../models/Caretaker");
 const User = require("../models/User");
@@ -10,23 +10,20 @@ app.get("/deleteuser", (req, res) => {
 });
 
 app.get("/deleteuser/confirm", (req, res) => {
-    Child.find({createdby: req.session.currentUser._id})
-    .then((children) => {
-        var promises = [];
-     for (i = 0; i < children.length; i++){
-        promises.push(cloudinary.uploader.destroy(children[i].profile_pic));
-     }
-     Promise.all(promises);
-    })
-    .then(() => Child.deleteMany({"createdby": req.session.currentUser._id})
-    )
-    .then(() => Caretaker.deleteMany({"createdby": req.session.currentUser._id})
-    )
-    .then(() => User.findByIdAndDelete(req.session.currentUser._id)
-    )
-    .then(() => res.redirect(`/`)
-    )
-    .catch(err => console.log(err));
+    Child.find({ createdby: req.session.currentUser._id })
+        .then((children) => {
+            var promises = [];
+            for (i = 0; i < children.length; i++) {
+                promises.push(cloudinary.uploader.destroy(children[i].profile_pic));
+            }
+            Promise.all(promises);
+        })
+        .then(() => Child.deleteMany({ "createdby": req.session.currentUser._id }))
+        .then(() => Caretaker.deleteMany({ "createdby": req.session.currentUser._id }))
+        .then(() => User.findByIdAndDelete(req.session.currentUser._id))
+        .then(() => req.session.destroy())
+        .then(() => res.redirect(`/`))
+        .catch(err => console.log(err));
 });
 
 module.exports = app;
