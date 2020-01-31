b869b7b0a0b9d5f1890f4c627efd801216cdafbc
almightyblob <dominik.antunovic@gmail.com>
2020-01-31 14:57:44 +0100

    saving before merge

--- routes/friendphoto.js
+++ routes/friendphoto.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const app = express();
 const Child = require("../models/Child");
-const multer = require("multer");
-const uploadCloud = require('../config/cloudinary');
-const createError = require('http-errors');
+const uploadCloud = require("../config/cloudinary");
 
 app.get("/friendphoto", (req, res) => {
-    res.render("friends/friendphoto");
+  res.render("friends/friendphoto");
 });
 
 app.post("/friendphoto", uploadCloud.single("profile_pic"), function(req, res) {
-    debugger;
-    Child.findByIdAndUpdate(req.session.childID, {
-            profile_pic: req.file.public_id
-        })
-        .then(() => {
-            res.redirect("/friends/addcaretaker");
-        })
-        .catch(err => console.log(err));
+  debugger;
+  Child.findByIdAndUpdate(req.session.childID, {
+    profile_pic: req.file.public_id
+  })
+    .then(() => {
+      res.redirect("/friends/addcaretaker");
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = app;
